e9089d9e2166b7b341a199b04b89fb05218d8a6f
jojo <polivanov004@gmail.com>
2024-09-25 15:04:16 +0300

    add token auth

--- core/settings.py
+++ core/settings.py
@@ -21,7 +21,7 @@ SECRET_KEY = '^l)7d*%h&db4uft@dk%h-w&nup#pu%)a!d)c7jwgoixo5_hm0$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 INSTALLED_APPS = [
@@ -39,7 +39,11 @@ INSTALLED_APPS = [
     
 ]
 
-
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
